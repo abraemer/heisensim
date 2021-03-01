840aeeb7d2c2ad72235ad67e412a34e742be3f5c
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2021-03-01 08:49:39 +0100

    Different rho ranges for different gometries

--- scripts/positions.py
+++ scripts/positions.py
@@ -24,9 +24,15 @@ def empty_position_set(rho, realizations, system_size, dim=3):
     )
     return dataset
 
+# cutoffs:
+#  d=1 rho=1.25
+#  d=2 rho=1.7
+#  d=3 rho=2.1
+N_SAMPLES = 20
+RHO_RANGES = [(0.1, 1.25), (0.1, 1.7), (0.1,2.1)]
 ## core function
 def create_positions(geometry, dim, N, disorder_realizations=100):
-    rho_list = np.round(np.arange(0.05, 0.95, 0.05), 2)
+    rho_list = np.round(np.linspace(*RHO_RANGES[dim-1], N_SAMPLES),2)#np.round(np.arange(0.05, 0.95, 0.05), 2)
     positions = empty_position_set(rho_list, disorder_realizations, N, dim)
 
     for rho in rho_list:
