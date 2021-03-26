a9491ec17892b2540afb8a94aaedc1a0a1ac1602
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2021-03-26 15:35:42 +0100

    sh schript to start scaling analysis

--- slurm/start_sa.sh
+++ slurm/start_sa.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch --output="logs/run-sa-N_$1-1d-alpha_6-slurm-%j.out" slurm/run_ed.slurm $1
+sbatch --output="logs/run-sa-N_$1-1d-alpha_6-slurm-%j.out" slurm/scaling_analysis.slurm $1
