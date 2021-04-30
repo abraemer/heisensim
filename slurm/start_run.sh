09b93e23ef6c5601dcaad4352291da0c0e407e18
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2021-04-30 13:22:40 +0200

    adapt other scripts to new geometry

--- slurm/start_run.sh
+++ slurm/start_run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sbatch --output="logs/run-ed-N_$1-$2d-alpha_$3-slurm-%j.out" slurm/run_ed.slurm $1 $2 $3
+if [! $# -eq 5]
+then
+    echo "Usage: start_run.sh N D ALPHA GEOM"
+    exit
+fi
+sbatch --output="logs/run-ed-N_$1-$2d-alpha_$3-slurm-%j.out" slurm/run_ed.slurm $1 $2 $3 $4
